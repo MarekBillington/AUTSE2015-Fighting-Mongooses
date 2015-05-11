d290ca5214255f15db8df7bb838be3e97131acd7
MarekBillington <marek.billington@gmail.com>
2015-05-12 00:58:07 +1200

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>Assignment 1</title>
+	<link href="style.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="webpage">
+			<div class="menu">
+				<div class="links"><a href="index.php">Home</a></div>
+				<div class="links"><a href="postform.php">Upload a document</a></div>
+				<div class="links"><a href="searchform.php">Search for a document</a></div>
+				<div class="links"><a href="postarticle.php">Write a new article</a></div>
+			</div>
+			<div class="content">
+				<div id="index">
+				<h1>SERLER</h1>
+				<hr>
+				<p>
+					Welcome to the SERLER system created by:<br>
+					1378882 Brad Wheeler<br>
+					1397762 Marek Billington<br>
+					1397794 Tom Clough<br>
+					<br>
+					<br>
+				</p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
