f7a3219a6684830ff21898861a4a1624b9e06ba4
MarekBillington <MarekBillington@users.noreply.github.com>
2015-06-07 23:41:29 +1200

    Index Page
    This is the homepage.

--- Index.php
+++ Index.php
@@ -1,3 +1,12 @@
+<?php
+    session_start();
+    
+    if(!isset($_SESSION['user'])){
+        $_SESSION['user'] = "";
+    }
+    
+    $user = $_SESSION['user'];
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -5,27 +14,37 @@
 	<link href="style.css" rel="stylesheet">
 	</head>
 	<body>
+		<div id='welcome'>
+			<div>
+			<img src="serl.jpg" width="100px" height="70px" align="float:left">
+			</div>
+		<?php
+	        if($user != ""){
+	            echo "<div align='float:left'><p>Welcome, $user    <a href='admin.php'>Dashboard</a>    <a href='logout.php'>Logout</a>    <a href='postarticle.php'>Add Article</a></p></div>";
+	        }
+	    ?>
+	    </div>
 		<div class="webpage">
-			<div class="menu">
-				<div class="links"><a href="index.php">Home</a></div>
-				<div class="links"><a href="postform.php">Upload a document</a></div>
-				<div class="links"><a href="searchform.php">Search for a document</a></div>
-				<div class="links"><a href="postarticle.php">Write a new article</a></div>
-			</div>
-			<div class="content">
-				<div id="index">
-				<h1>SERLER</h1>
-				<hr>
-				<p>
-					Welcome to the SERLER system created by:<br>
-					1378882 Brad Wheeler<br>
-					1397762 Marek Billington<br>
-					1397794 Tom Clough<br>
-					<br>
-					<br>
-				</p>
-				</div>
-			</div>
+		<div class="menu">
+		<div class="links"><a href="index.php">Home</a></div>
+		<div class="links"><a href="login.php">Log In</a></div>
+		<div class="links"><a href="searchform.php">Search for a Document</a></div>
+		<div class="links"><a href="about.php">About this Website</a></div>
+		</div>
+		<div class="content">
+		<div id="index">
+		<h1>SERLER</h1>
+		<hr>
+		<p>
+			Welcome to the Software Engineering Research Lab(SERLER) at AUT.<br>
+			This repository has been created for the use of capturing evidence relating<br>
+			to software development methods. The overarching goal of this system is to<br>
+			support researchers and practitioners in decision-making during software projects.<br>
+		<br>
+		<br>
+		</p>
+		</div>
+		</div>
 		</div>
 	</body>
 </html>
